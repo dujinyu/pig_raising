3f53dc08a92b8a066504b0c6ffcd8cbe3bc909b0
dujinyu <1242642280@qq.com>
2018-11-18 18:46:30 +0800

    初始化了music player项目，并且初始化了项目的样式

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+//引入初始化样式和防止边框在高清手机显示问题的边框样式
+import './common/css/reset.css'
+import './common/css/border.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
